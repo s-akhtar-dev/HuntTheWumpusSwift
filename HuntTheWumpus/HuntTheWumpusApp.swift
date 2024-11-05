ed57930701237f667f69d0cbfaf8e93d7cd2b571
Sarah Akhtar <s_akhtar@u.pacific.edu>
2024-11-04 22:41:35 -0800

    Initial Commit

---
+++ HuntTheWumpus/HuntTheWumpusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuntTheWumpusApp.swift
+//  HuntTheWumpus
+//
+//  Created by Sarah Akhtar on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HuntTheWumpusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
